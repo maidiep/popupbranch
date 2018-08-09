6b91c675bdd3e79f1d8771182668003c777f890a
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-08-09 02:22:40 -0400

    pushing prior to trying facebook login authentication

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import "./Products.css";
 
 const Products = () => (
 <div>
+<div className="intro-text left-0 text-centerfaded rounded text-center">
+<h2 className="section-heading">
+              <span id="instagram" className="section-heading-lower">
+                Shop Our Products
+              </span>
+            </h2>
+            </div>
      <div className="page-section">
         <div className="container">
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex mr-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">Roses</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">&nbsp;&nbsp;&nbsp;Roses&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;<i className="fa fa-shopping-cart"></i></h2>
+                        
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/red.jpeg" alt="roses"/></div>
         </div>
@@ -17,7 +26,7 @@ const Products = () => (
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex ml-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">carnations</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Carnations</span><span><i className="fa fa-shopping-cart"></i></span></h2>
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/orange.jpeg" alt="carnations"/></div>
         </div>
@@ -27,7 +36,7 @@ const Products = () => (
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex mx-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">mums</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Mums</span><i className="fa fa-shopping-cart"></i></h2>
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/mums.jpeg" alt="mums"/></div>
         </div>
@@ -37,7 +46,7 @@ const Products = () => (
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex mr-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">succulents</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Succulents</span><i className="fa fa-shopping-cart"></i></h2>
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/succulents.jpeg" alt="succulents"/></div>
         </div>
@@ -47,7 +56,7 @@ const Products = () => (
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex ml-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">hydrangeas</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Hydrangeas</span><i className="fa fa-shopping-cart"></i></h2>
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/hydrangeas.jpeg" alt="hydrangeas"/></div>
         </div>
@@ -57,7 +66,7 @@ const Products = () => (
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex mx-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">orchids</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Orchids</span><i className="fa fa-shopping-cart"></i></h2>
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/orchid.jpeg" alt="orchids"/></div>
         </div>
@@ -67,12 +76,13 @@ const Products = () => (
             <div className="product-item">
                 <div className="d-flex product-item-title">
                     <div className="d-flex mr-auto bg-faded p-5 rounded">
-                        <h2 className="div-heading mb-0"><span className="div-heading-lower">lavander</span><i className="fa fa-shopping-cart"></i></h2>
+                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Lavander</span><i className="fa fa-shopping-cart"></i></h2>
                     </div>
                 </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/lavender.jpeg" alt="lavender"/></div>
         </div>
     </div>
 </div>
+
 );
 
 export default Products;
