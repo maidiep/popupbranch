a7761cd628cb7d0253a2ce4e352218c5f305b8e6
Kacie Nguyen <maidiep.nguyen@hotmail.com>
2018-08-09 20:01:25 -0400

    componentize products

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import "./index.css";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import Navbar from "./components/Navbar";
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,88 +1,65 @@
-import React from 'react';
+import React from "react";
 import "./Products.css";
+import Item from "../Item";
+
+const items = [
+  {
+    title: "Roses",
+    image: "assets/img/red.jpeg",
+    position: "mr-auto"
+  },
+
+  {
+    title: "Carnations",
+    image: "assets/img/orange.jpeg",
+    position: "mx-auto"
+  },
+
+  {
+    title: "Mums",
+    image: "assets/img/mums.jpeg",
+    position: "ml-auto"
+  },
+
+  {
+    title: "Succulents",
+    image: "assets/img/succulents.jpeg",
+    position: "mr-auto"
+  },
+
+  {
+    title: "Hydrangeas",
+    image: "assets/img/hydrangeas.jpeg",
+    position: "mx-auto"
+  },
+
+  {
+    title: "Orchids",
+    image: "assets/img/orchid.jpeg",
+    position: "ml-auto"
+  },
+
+  {
+    title: "Lavender",
+    image: "assets/img/lavender.jpeg",
+    position: "mr-auto"
+  }
+];
 
 const Products = () => (
-<div>
-<div className="intro-text left-0 text-centerfaded rounded text-center">
-<h2 className="section-heading">
-              <span id="instagram" className="section-heading-lower">
-                Shop Our Products
-              </span>
-            </h2>
-            </div>
-     <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex mr-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">&nbsp;&nbsp;&nbsp;Roses&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;<i className="fa fa-shopping-cart"></i></h2>
-                        
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/red.jpeg" alt="roses"/></div>
-        </div>
+  <div>
+    <div className="intro-text left-0 text-centerfaded rounded text-center">
+      <h2 className="section-heading">
+        <span id="instagram" className="section-heading-lower">
+          Shop Our Products
+        </span>
+      </h2>
     </div>
-    <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex ml-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Carnations</span><span><i className="fa fa-shopping-cart"></i></span></h2>
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/orange.jpeg" alt="carnations"/></div>
-        </div>
-    </div>
-    <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex mx-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Mums</span><i className="fa fa-shopping-cart"></i></h2>
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/mums.jpeg" alt="mums"/></div>
-        </div>
-    </div>
-    <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex mr-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Succulents</span><i className="fa fa-shopping-cart"></i></h2>
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/succulents.jpeg" alt="succulents"/></div>
-        </div>
-    </div>
-    <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex ml-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Hydrangeas</span><i className="fa fa-shopping-cart"></i></h2>
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/hydrangeas.jpeg" alt="hydrangeas"/></div>
-        </div>
-    </div>
-    <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex mx-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Orchids</span><i className="fa fa-shopping-cart"></i></h2>
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/orchid.jpeg" alt="orchids"/></div>
-        </div>
-    </div>
-    <div className="page-section">
-        <div className="container">
-            <div className="product-item">
-                <div className="d-flex product-item-title">
-                    <div className="d-flex mr-auto bg-faded p-5 rounded">
-                        <h2 className="section-heading mb-0"><span className="section-heading-lower">Lavander</span><i className="fa fa-shopping-cart"></i></h2>
-                    </div>
-                </div><img className="img-fluid d-flex mx-auto product-item-img mb-3 mb-lg-0 rounded" src="assets/img/lavender.jpeg" alt="lavender"/></div>
-        </div>
-    </div>
-</div>
 
+    {items.map(item => (
+      <Item title={item.title} image={item.image} position={item.position} />
+    ))}
+  </div>
 );
 
 export default Products;
